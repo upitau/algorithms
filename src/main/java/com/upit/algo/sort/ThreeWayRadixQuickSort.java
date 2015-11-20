e605085558f22f39d52bbbff9c8bc54e40472eb8
Yury Pitsishin <upitau@gmail.com>
2015-11-20 22:33:02 +1100

    Add Three Way Radix Quick Sort

---
+++ src/main/java/com/upit/algo/sort/ThreeWayRadixQuickSort.java
@@ -0,0 +1,47 @@
+package com.upit.algo.sort;
+
+public class ThreeWayRadixQuickSort {
+    private static int RADIX = 256;
+
+    public void sort(String[] words) {
+        sort(words, 0, words.length - 1, 0);
+    }
+
+    private void sort(String[] words, int lo, int hi, int charIndex) {
+        if (hi <= lo) {
+            return;
+        }
+
+        int pivot = charAt(words[lo], charIndex);
+        int lt = lo;
+        int gt = hi;
+
+        int i = lo + 1;
+        while (i <= gt) {
+            int ch = charAt(words[i], charIndex);
+            if (ch < pivot) {
+                swap(words, lt++, i++);
+            } else if (ch > pivot) {
+                swap(words, i, gt--);
+            } else {
+                i++;
+            }
+        }
+
+        if (pivot >= 0) {
+            sort(words, lo, lt - 1, charIndex);
+            sort(words, lt, gt, charIndex + 1);
+        }
+        sort(words, gt + 1, hi, charIndex);
+    }
+
+    private int charAt(String word, int index) {
+        return index < word.length() ? word.charAt(index) : -1;
+    }
+
+    private void swap(String[] words, int a, int b) {
+        String temp = words[a];
+        words[a] = words[b];
+        words[b] = temp;
+    }
+}
